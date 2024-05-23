a0313e70c8208cb76879975a2b74a41955e29e16
Renato <renatoaferreirafilho@gmail.com>
2024-05-23 05:20:54 -0300

    Atualizando nome da tabela do BD

--- repositories/ProcessaDadosFinanciamento.repository.js
+++ repositories/ProcessaDadosFinanciamento.repository.js
@@ -3,7 +3,7 @@ import { connect } from "./db.js";
 async function getIndiceInflacao(indice, data) {
     const conn = await connect();
     try {
-        const res = await conn.query(`SELECT ${indice} FROM indices WHERE data = ${data}`);
+        const res = await conn.query(`SELECT ${indice} FROM indices_de_correcao WHERE data = ${data}`);
 
         return res.rows;
     } catch (err) {
